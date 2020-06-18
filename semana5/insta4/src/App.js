68feaf033d65f4b214560ec426f364fe9845f8b5
unknown <glhrmpaiva@gmail.com>
2020-06-18 18:03:29 -0300

    Terminado exercicio de implementação 2

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -8,6 +8,15 @@ display: flex;
   flex-direction: column;
   align-items: center;
 `
+const Formulario = styled.div`
+  border: 1px solid gray;
+  width: 280px;
+  margin: 10px 0;
+  padding: 10px;
+`
+const BotaoForm = styled.button`
+  margin-left: 10px;
+`
 
 class App extends React.Component {
   state = {
@@ -27,8 +36,36 @@ class App extends React.Component {
           fotoUsuario: "https://picsum.photos/50/51",
           fotoPost: "https://picsum.photos/200/149"
         }
-    ]
+    ],
+    valorInputNome: "",
+    valorFotoUsuario: "",
+    valorFotoPost: ""
+
   }
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorFotoUsuario,
+      fotoPost: this.state.valorFotoPost
+    }
+    const newPosts = [...this.state.Posts, novoPost] 
+    this.setState({Posts: newPosts})
+  }
+
+  onChangeInputNome = event => {
+    this.setState({valorInputNome: event.target.value})
+  }
+
+  onChangeFotoUsuario = event => {
+    this.setState({valorFotoUsuario: event.target.value})
+  }
+
+  onChangeFotoPost = event => {
+    this.setState({valorFotoPost: event.target.value})
+  }
+
+
 
   render() {
     const listaDePosts = this.state.Posts.map(post => {
@@ -42,8 +79,29 @@ class App extends React.Component {
     });
     return (
       <AppContainer>
+         <Formulario>
+          <h3>Adicione uma nova foto</h3>
+          <input 
+          placeholder="Nome" 
+          value={this.state.valorInputNome}
+          onChange={this.onChangeInputNome}
+          />
+          <input 
+          placeholder="Foto usuário"
+          value={this.state.valorFotoUsuario}
+          onChange={this.onChangeFotoUsuario}
+          />
+          <input
+          placeholder="Foto post"
+          value={this.state.valorFotoPost}
+          onChange={this.onChangeFotoPost}
+          />
+          <BotaoForm onClick={this.adicionaPost}>Postar</BotaoForm>
+         </Formulario>
+
          <div>{listaDePosts}</div>
       </AppContainer>
+    
     );
   }
 }
