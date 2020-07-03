0676dfd9498d33f30ed0d6b02fdea12c796c9882
unknown <glhrmpaiva@gmail.com>
2020-07-03 16:58:44 -0300

    Primeira entrega -incompleta- do Labefy

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -1,26 +1,166 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import {Page, Header, Sidebar, Main, CriarPlaylist, LabelCriarPlaylist, InputCriaPlaylist, 
+  BotaoCriaPlaylist, ListaPlaylists, LabelPlaylists, CadaPlaylist, LinhaPlaylist, BotaoHome, 
+  BotaoDeletar, PlaylistNome, LinhaMusica, LinhaMusicaArtista, QuebraEntreMusicas, TituloAdicionar,
+} from './components/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axiosConfig = {
+  headers: {
+    Authorization: "guilherme-paiva-turing"
+  }
+}
+
+export class App extends React.Component {
+  state = {
+    playlists: [],
+    musicas: [],
+    valorInputPlaylist:"",
+    tituloPlaylist: 'Teste',
+    mostraHome: true,
+  }
+
+  componentDidMount = () => {
+    this.pegarPlaylists()
+  }
+
+  adicionaPlaylist = () => {
+    const body = {
+      name: this.state.valorInputPlaylist
+    }
+    axios.post(
+      "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+      body, axiosConfig
+    )
+    .then(() => {
+      this.setState({valorInputPlaylist: ""})
+      this.pegarPlaylists()
+    })
+    .catch((error) => {
+      alert("Falha ao criar playlist")
+      console.log(error.message)
+    })
+  }
+
+  pegarPlaylists = () => {
+    axios.get(
+      "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+      axiosConfig
+    )
+    .then((response) => {
+      this.setState({playlists: response.data.result.list})
+    })
+    .catch((error) => {
+      alert("Falha ao pegar playlists")
+      console.log(error.message)
+    })
+  }
+
+  deletarPlaylist = (playlistid) => {
+    axios.delete(
+      `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistid}`,
+      axiosConfig
+    )
+    .then(() => {
+      this.pegarPlaylists()
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
+  }
+
+  doisEmUm = (playlist) => {
+    
+      this.setState({mostraHome: false})
+      axios.get(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlist.id}/tracks`,
+        axiosConfig
+      )
+      .then((response) => {
+        this.setState({musicas: response.data.result.tracks})
+      })
+      .catch((error) => {
+        alert("Falha ao exibir músicas da playlist")
+        console.log(error.message)
+      })
+    
+
+    this.setState({tituloPlaylist: playlist.name})
+  }
+
+  
+
+  onChangeInputPlaylist = event => {
+    this.setState({valorInputPlaylist: event.target.value})
+  }
+
+  render(){
+    console.log(this.state.tituloPlaylist)
+    const trocaTela = () => {
+      if (this.state.mostraHome){
+        return(
+          <p>Bem vindo ao Labefy, selecione ao lado a playlist que deseja ouvir</p>
+        )}else{
+          return(
+            <div>
+            <PlaylistNome>{this.state.tituloPlaylist}</PlaylistNome>
+            <LinhaMusicaArtista>M Ú S I C A<span>A R T I S T A</span></LinhaMusicaArtista>
+            <hr />
+            {this.state.musicas.map((musica) => {
+              return(
+                  <div><LinhaMusica key={musica.id}>
+                      {musica.name}<span>{musica.artist}</span>
+                  </LinhaMusica><QuebraEntreMusicas />
+                  </div>
+              )
+          })}
+          <div>
+              <TituloAdicionar>Adicionar músicas à playlist:</TituloAdicionar>
+              <input placeholder="Nome da Música"></input>
+              <input placeholder="Nome do Artista"></input>
+              <input placeholder="Link da Música"></input>
+              <button>Adicionar Música</button>
+            </div>
+          </div>
+          ) 
+       }
+     } 
+
+     return (
+      <Page>
+        <Header>Labefy</Header>
+        <Sidebar>
+          <BotaoHome onClick={() => {this.setState({mostraHome: true})}}>Home</BotaoHome>
+          <ListaPlaylists>
+            <LabelPlaylists>Playlists</LabelPlaylists>
+            <CadaPlaylist>
+              {this.state.playlists.map((playlist) => {
+                return (
+                <LinhaPlaylist onClick={() => {this.doisEmUm(playlist)}} key={playlist.id}>
+                  {playlist.name}
+                <BotaoDeletar onClick={() => {this.deletarPlaylist(playlist.id)}}>x</BotaoDeletar>
+                </LinhaPlaylist>
+              )})}
+            </CadaPlaylist>
+          </ListaPlaylists>
+          <CriarPlaylist>
+            <LabelCriarPlaylist>Criar nova playlist:</LabelCriarPlaylist>
+            <InputCriaPlaylist 
+            placeholder="Nome da Playlist"
+            onChange={this.onChangeInputPlaylist}
+            value={this.state.valorInputPlaylist}
+            />
+            <BotaoCriaPlaylist 
+            onClick={this.adicionaPlaylist}>Criar</BotaoCriaPlaylist>
+          </CriarPlaylist>
+        </Sidebar>
+        <Main>
+          {trocaTela()}
+            
+        </Main>
+      </Page>
+    );
+  }
 }
 
 export default App;
---
+++ semana7/labefy/src/components/styles.js
@@ -0,0 +1,109 @@
+import styled from 'styled-components'
+
+   export const Page = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+  `
+  export const Header = styled.div`
+    background-color: #1ED561;
+    color: white;
+    height: 8.5vh;
+    width: 100vw;
+    font-size: 50px;
+    font-weight: bold;
+    text-align: center;
+  `
+  export const Sidebar = styled.div`
+    background-color: rgb(18, 18, 18);
+    color: rgb(179, 179, 179);
+    height: 91.5vh;
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+  `
+  export const Main = styled.div`
+    background-color: rgb(24, 24, 24);
+    color: rgb(237, 237, 237);
+    flex-grow: 1;
+    height: 91.5vh;
+    flex-shrink: 10;
+    padding: 50px;
+  `
+  export const CriarPlaylist = styled.div`
+    margin: 0 10px 10px 10px;
+    font-weight: bold;
+    justify-self: flex-end;
+  `
+  export const LabelCriarPlaylist = styled.label`
+    font-size: 17px;
+    text-align: left;
+    margin-left: 10px;
+  `
+  export const InputCriaPlaylist = styled.input`
+    width: 100px;
+    font-size: 12px;
+    background-color: black;
+    color: rgb(179, 179, 179);
+    margin: 6px 3px 0 10px;
+  `
+  export const BotaoCriaPlaylist = styled.button`
+    width: 45px;
+    background-color: black;
+    color: rgb(179, 179, 179);
+  `
+  export const ListaPlaylists = styled.label`
+    margin: 10px;
+  `
+  export const LabelPlaylists = styled.label`
+    font-size: 17px;
+    text-align: left;
+    font-weight: bold;
+    margin-left: 10px;
+  `
+  export const CadaPlaylist = styled.ul`
+    list-style: none;
+    margin: 15px -30px;
+  `
+  export const LinhaPlaylist = styled.li`
+    margin: 10px 0 0 0;
+    width: 152px;
+    display: flex;
+    justify-content: space-between;
+  `
+  export const BotaoHome = styled.label`
+    font-size: 17px;
+    text-align: left;
+    font-weight: bold;
+    margin: 20px;
+  `
+  export const BotaoDeletar = styled.span`
+    color: rgb(179, 179, 179); 
+  `
+  export const PlaylistNome = styled.h2`
+  font-size: 70px;
+  margin: 0 0 30px 0;
+  `
+  export const LinhaMusica = styled.li`
+  margin: 10px 0 10px 4px;
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  `
+  export const LinhaMusicaArtista = styled.p`
+  margin: 30px 0 0 4px;
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  `
+  export const QuebraEntreMusicas = styled.hr`
+  opacity: 20%;
+  border: none;
+  height: 1px;
+  color: rgb(179, 179, 179);
+  background-color: rgb(179, 179, 179);
+  `
+  export const TituloAdicionar = styled.h3`
+  margin: 40px 0 5px 0;
+  `
